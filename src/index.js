3b504c387e8756a8816d61fde76f6aefb88ec5fc
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-22 15:02:06 -0700

    Create initial state

--- src/index.js
+++ src/index.js
@@ -4,24 +4,6 @@ import './index.css';
 import App from './components/App.js';
 import store from './store';
 
-const stories = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  }, {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const storiesData = store.getState();
 
 ReactDOM.render(
