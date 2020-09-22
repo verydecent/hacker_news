d8d527327cee4a1af50ce6d322ac5e76d0a2b930
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-22 13:03:29 -0700

    Pass props to Stories component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Stories from './Stories';
 
-const App = () => {
+const App = ({
+  stories
+}) => {
   return (
     <div className=''>
       App
+      <Stories stories={stories} />
     </div>
   );
 }
