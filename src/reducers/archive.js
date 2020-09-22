7e29cf2fbd560d5c6f178ab132de4b8ebb9b9301
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-22 15:35:55 -0700

    Import STORY_ARCHIVE constant and create immutable pure function that returns new copy of array with previous state and new item

--- src/reducers/archive.js
+++ src/reducers/archive.js
@@ -1,7 +1,15 @@
+import { STORY_ARCHIVE } from '../constants/actionsTypes';
+
 const INTIAL_STATE = [];
+
+const applyArchiveStory = (state, action) =>
+  [...state, action.id];
 
 function archiveReducer(state = INTIAL_STATE, action) {
   switch (action.type) {
+    case STORY_ARCHIVE: {
+      return applyArchiveStory(state, action);
+    }
     default: return state;
   }
 }
