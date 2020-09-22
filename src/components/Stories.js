a53a57ce7424e59ac025f0b37c175a3690e1d5be
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-22 13:16:44 -0700

    pass props

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -2,10 +2,37 @@ import React from 'react';
 import Story from './Story';
 import './Stories.css';
 
+const COLUMNS = {
+  title: {
+    label: 'Title',
+    width: '40%'
+  },
+  author: {
+    label: 'Title',
+    width: '40%'
+  },
+  comments: {
+    label: 'Title',
+    width: '40%'
+  },
+  points: {
+    label: 'Title',
+    width: '40%'
+  },
+  archive: {
+    label: 'Title',
+    width: '40%'
+  },
+};
+
 const Stories = ({ stories }) =>
   <div className='stories'>
     {(stories || []).map(story =>
-      <Story key={story.objectID} story={story} />
+      <Story
+        key={story.objectID}
+        story={story}
+        columns={COLUMNS}
+      />
     )}
   </div>
 
