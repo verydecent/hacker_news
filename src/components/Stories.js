1236e6f3bbfcfa8ee2f5d7cab01377edd04a1317
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-22 13:05:54 -0700

    Receive stories props and map out stories props with Story component

---
+++ src/components/Stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Story from './Story';
+
+const Stories = ({ stories }) =>
+  <div className=''>
+    {(stories || []).map(story =>
+      <Story key={story.objectID} story={story} />
+    )}
+  </div>
+
+export default Stories;
